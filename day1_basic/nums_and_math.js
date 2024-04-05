ff9a380d823f94af029a8ebba92038423c9875d4
ShreyasiGhoshOfficial <142476525+ShreyasiGhoshOfficial@users.noreply.github.com>
2024-04-05 19:09:54 +0000

    day1

---
+++ day1_basic/nums_and_math.js
@@ -0,0 +1,13 @@
+const num = new Number(100)
+console.log(num);
+console.log(num.toString().length);
+console.log(num.toFixed(2));
+//toPrecision
+//toLocaleString eg: 1,00,000
+//-------------Math------------------------------
+//Math.abs-->Math.round-->Math.ceil-->Math.floor
+
+console.log(Math.round(4.5));
+//Important
+//Math.random -->range[0,1]
+console.log(Math.random());
