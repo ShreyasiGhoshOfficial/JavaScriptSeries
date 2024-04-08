da0f9792993a181443d1c9968337b936166865a0
ShreyasiGhoshOfficial <142476525+ShreyasiGhoshOfficial@users.noreply.github.com>
2024-04-08 11:29:13 +0000

    day2

---
+++ day2_basic/array.js
@@ -0,0 +1,24 @@
+// array in js is Oth index
+// in ja array copy data as shallow copy
+
+
+const arr=[1,2,3,4,5]
+console.log(arr[0]);
+arr.push(6);
+arr.push(7);
+arr.pop();
+console.log(arr);
+console.log(arr.indexOf(4));
+
+const newarr = arr.join();// convert into string using previous array value;
+console.log(arr);
+console.log(newarr);
+//-------------------------Slice,splice-------------
+
+const arr2 = arr.slice(1,3); // slice take character from index 1 to 2 only not take 3
+console.log(arr2);
+
+const arr3 = arr.splice(1,3); // splice take character from index of 1 to 3 
+console.log(arr);// here splice remove element from specific index
+
+console.log(arr3);
